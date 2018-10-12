30c705c5ebadadea34a7e0225c00ce2db0619fae
Ray Lomeli <raylom96@gmail.com>
2018-10-12 11:45:55 -0500

    added project and action endpoints

--- index.js
+++ index.js
@@ -23,5 +23,26 @@ server.get("/projects", (req, res) => {
     .catch(err => res.status(500).json({ error: "No Projects Coming :(" }));
 });
 
+server.get("/actions", (req, res) => {
+  actionDb
+    .get()
+    .then(action => {
+      res.status(200).json(action);
+    })
+    .catch(error => res.status(500).json({ error: "Can't get actions..." }));
+});
+
+server.get("/projects/:id", (req, res) => {
+  projectDb
+    .getProjectActions(req.params.id)
+    .then(proj => {
+      if (proj === 0) {
+        res.status(400).json("no projects by that id");
+      }
+      res.status(200).json(proj);
+    })
+    .catch(error => res.status(500).send({ error: "You messed up" }));
+});
+
 const port = 8001;
 server.listen(port, () => console.log(`API running on port ${port}`));
